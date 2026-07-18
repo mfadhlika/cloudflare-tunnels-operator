1291330d499511d18ee36226e21b8ce30a985514
Muhammad Fadhlika <git@fadhlika.com>
2026-07-18 13:06:45 +0700

    at least one

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -328,7 +328,7 @@ async fn test_ingress_controller() {
 
     tokio::time::sleep(Duration::from_secs(30)).await;
 
-    list_tunnel_mock.assert_async().await;
+    list_tunnel_mock.expect_at_least(1).assert_async().await;
     list_dns_mock.assert_async().await;
     create_cname_mock.assert_async().await;
     create_txt_mock.assert_async().await;
